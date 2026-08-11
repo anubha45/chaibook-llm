571919300d266c5d74d83688a3207532f8846992
anubha45 <anubhavgupta0910@gmail.com>
2026-08-11 12:45:51 +0530

    applying crud done

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,10 +2,20 @@ import express from "express";
 import "dotenv/config";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth.js";
-
+import cors from "cors";
+import { registerRoutes } from "./routes/index.js";
+import { errorHandler } from "./middleware/error-handler.middleware.js";
 
 const app = express();
 const PORT = process.env.PORT || 8081;
+const clientUrl = process.env.CLIENT_URL || "http://localhost:3000";
+
+app.use(
+    cors({
+      origin: clientUrl,
+      credentials: true,
+    })
+)
 
 
 app.all('/api/auth/{*any}', toNodeHandler(auth));
@@ -19,6 +29,9 @@ app.get("/", (req, res) => {
 app.get("/health", (req, res) => {
   res.send("Server is healthy!");
 });
+
+registerRoutes(app);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
